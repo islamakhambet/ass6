c4c2b80b1cbbe4e31a86cd1d5637c61f92fd8fa3
islamakhambet <77294016+islamakhambet@users.noreply.github.com>
2021-03-13 15:56:33 +0600

    Delete ass6 directory

--- ass6/src/Builder.java
+++
@@ -1,9 +0,0 @@
-public interface Builder {
-    public void buildHouseWalls();
-    public void buildHouseDoors();
-    public void buildHouseWindows();
-    public void buildHouseRoof();
-    public void buildHouseGarage();
-    public House getHouse();
-
-}
--- ass6/src/HousePlan.java
+++
@@ -1,7 +0,0 @@
-public interface HousePlan {
-    public void setWalls(String walls);
-    public void setDoors(String doors);
-    public void setWindows(String windows);
-    public void setRoof(String roof);
-    public void setGarage(String garage);
-}
