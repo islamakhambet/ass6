af598fc23ff05ceb09ad712ce1dd4130c4031142
islamakhambet <77294016+islamakhambet@users.noreply.github.com>
2021-03-13 14:40:46 +0600

    Update Application.java

--- ass6/src/task1/Application.java
+++ ass6/src/task1/Application.java
@@ -1,4 +1,11 @@
 package task1;
 
 public class Application {
+   public static void main(String[] args) {
+     Database foo=Database.getInstance();
+            foo.query("SELECT...");
+            Database bar=Database.getInstance();
+            bar.query("SELECT...");
+
+        }
 }
