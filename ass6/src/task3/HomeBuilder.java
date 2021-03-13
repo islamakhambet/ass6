1df33942d85cdf0950bef7e8b332d87e4f39e365
islamakhambet <77294016+islamakhambet@users.noreply.github.com>
2021-03-13 15:18:47 +0600

    Update HomeBuilder.java

--- ass6/src/task3/HomeBuilder.java
+++ ass6/src/task3/HomeBuilder.java
@@ -2,7 +2,7 @@ package task3;
 
 public class HomeBuilder {
    public Builder getBuild(String buildType){
-        if(buildType.equalsIgnoreCase("")){
+        if(buildType.equalsIgnoreCase(" Appartment")){
             return new Appartment();
         }else if (animalType.equalsIgnoreCase("Cottedge")){
             return new Cottedge();
