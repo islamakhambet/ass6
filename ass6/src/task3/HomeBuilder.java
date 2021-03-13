e70c9f8de1c597f6290c0a176bbcb78a7b96f0ae
islamakhambet <77294016+islamakhambet@users.noreply.github.com>
2021-03-13 15:12:27 +0600

    Update HomeBuilder.java

--- ass6/src/task3/HomeBuilder.java
+++ ass6/src/task3/HomeBuilder.java
@@ -1,4 +1,13 @@
 package task3;
 
 public class HomeBuilder {
+   public Builder getBuild(String buildType){
+        if(buildType.equalsIgnoreCase("")){
+            return new Appartment();
+        }else if (animalType.equalsIgnoreCase("Cottedge")){
+            return new Cottedge();
+        }else {
+            throw new RuntimeException("Unknown type of build");
+        }
+    }
 }
