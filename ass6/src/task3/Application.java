36c932b4946e4a292b0dfdce5f7522408f5b8a79
islamakhambet <77294016+islamakhambet@users.noreply.github.com>
2021-03-13 15:18:19 +0600

    Update Application.java

--- ass6/src/task3/Application.java
+++ ass6/src/task3/Application.java
@@ -1,4 +1,11 @@
 package task3;
 
 public class Application {
+  public static void main(String[] args) {
+        BuildApplication buildApp = new BuildApplication();
+        build build1 = buildApp.getbuild("Appartment");
+        build1.buildDoes();
+         build2 = buildApp.getbuild("Cottedge");
+         build2.buildDoes();
+    }
 }
